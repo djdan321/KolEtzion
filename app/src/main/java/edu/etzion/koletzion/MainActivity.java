7b175ddbd95ca77cc1bfe089605d80dfd94822b2
ya19 <appoyossi@gmail.com>
2019-03-28 19:13:50 +0200

    fixed bugs

--- app/src/main/java/edu/etzion/koletzion/MainActivity.java
+++ app/src/main/java/edu/etzion/koletzion/MainActivity.java
@@ -15,12 +15,12 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.viewpager.widget.ViewPager;
-import edu.etzion.koletzion.Adapters.ViewPagerAdapter;
-import edu.etzion.koletzion.Fragments.BroadcastersListFragment;
-import edu.etzion.koletzion.Fragments.FeedFragment;
-import edu.etzion.koletzion.Fragments.PersonalAreaFragment;
+
+
+
+import edu.etzion.koletzion.Fragments.MainViewPagerFragment;
+
 import edu.etzion.koletzion.authentication.AuthenticationActivity;
-import edu.etzion.koletzion.models.Profile;
 import edu.etzion.koletzion.player.ExoPlayerFragment;
 
 public class MainActivity extends AppCompatActivity
@@ -37,10 +37,10 @@ public class MainActivity extends AppCompatActivity
 		setContentView(R.layout.activity_main);
 		//method that includes all the FindViewById
 		findviews();
-		if (auth.getCurrentUser() == null) {
-			startActivity(new Intent(this, AuthenticationActivity.class).
-					addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
-		}
+//		if (auth.getCurrentUser() == null) {
+//			startActivity(new Intent(this, AuthenticationActivity.class).
+//					addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+//		}
 		setSupportActionBar(toolbar);
 		
 		getSupportFragmentManager().beginTransaction().replace(frame.getId(),
@@ -62,12 +62,7 @@ public class MainActivity extends AppCompatActivity
     }
 
 
-    private void findviews() {
-        frame = findViewById(R.id.frame);
-        toolbar = findViewById(R.id.toolbar);
 
-        playerFragment = new ExoPlayerFragment();
-    }
 
     @Override
     public void onBackPressed() {
@@ -78,94 +73,13 @@ public class MainActivity extends AppCompatActivity
             finish();
         }
     }
-    public void setBackStack(boolean bol){
-        System.out.println(bol);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-//            vpMain.setCurrentItem(0);
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
-    }
-    public void initPlayer(String filePath){
-        playerFragment.initPlayer(filePath);
-    }
 
 		
+
 		
-		ViewPagerAdapterMainActivity();
-		
-		
-		DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-		ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-				this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-		drawer.addDrawerListener(toggle);
-		toggle.syncState();
-		
-		NavigationView navigationView = findViewById(R.id.nav_view);
-		navigationView.setNavigationItemSelectedListener(this);
-	}
+
 	
-	private void ViewPagerAdapterMainActivity() {
-		//this method includes the viewpager adapter that includes all the mainactivity fragments.
-		ViewPagerAdapter vpMainAdapter = new ViewPagerAdapter(getSupportFragmentManager());
-		vpMainAdapter.addFragment(PersonalAreaFragment.newInstance(/*todo get from firebsae*/
-				new Profile("yair", "frid")), "PersonalAreaFragment");
-		vpMainAdapter.addFragment(new BroadcastersListFragment(), "BroadcastersListFragment");
-		vpMainAdapter.addFragment(new FeedFragment(), "FeedFragment");
-		
-		vpMain.setAdapter(vpMainAdapter);
-		vpMain.setCurrentItem(2);
-	}
-	
-	public static void test(int id) {
-		
-		vpMain.setCurrentItem(2, false);
-		
-	}
-	
+
 	private void findviews() {
 		auth = FirebaseAuth.getInstance();
 		frame = findViewById(R.id.frame);
@@ -173,16 +87,7 @@ public class MainActivity extends AppCompatActivity
 		vpMain = findViewById(R.id.vpMain);
 		playerFragment = new ExoPlayerFragment();
 	}
-	
-	@Override
-	public void onBackPressed() {
-		DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-		if (drawer.isDrawerOpen(GravityCompat.START)) {
-			drawer.closeDrawer(GravityCompat.START);
-		} else {
-			super.onBackPressed();
-		}
-	}
+
 	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -197,37 +102,37 @@ public class MainActivity extends AppCompatActivity
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
-		
+
 		//noinspection SimplifiableIfStatement
 		if (id == R.id.action_settings) {
 			return true;
 		}
-		
+
 		return super.onOptionsItemSelected(item);
 	}
-	
+
 	@SuppressWarnings("StatementWithEmptyBody")
 	@Override
 	public boolean onNavigationItemSelected(MenuItem item) {
 		// Handle navigation view item clicks here.
 		int id = item.getItemId();
-		
+
 		if (id == R.id.nav_camera) {
 			// Handle the camera action
 			vpMain.setCurrentItem(0);
 		} else if (id == R.id.nav_gallery) {
-		
+
 		} else if (id == R.id.nav_slideshow) {
-		
+
 		} else if (id == R.id.nav_manage) {
-		
+
 		}
-		
+
 		DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
 		drawer.closeDrawer(GravityCompat.START);
 		return true;
 	}
-	
+
 	public void initPlayer(String filePath) {
 		playerFragment.initPlayer(filePath);
 	}
