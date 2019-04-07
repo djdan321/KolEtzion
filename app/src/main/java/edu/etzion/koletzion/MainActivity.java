6ddbeda27d8fccb2b387474eed4be231f9e84bad
Fongeme <fridyair@gmail.com>
2019-04-07 12:07:16 +0300

    sas

--- app/src/main/java/edu/etzion/koletzion/MainActivity.java
+++ app/src/main/java/edu/etzion/koletzion/MainActivity.java
@@ -3,8 +3,6 @@ package edu.etzion.koletzion;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.FrameLayout;
 
@@ -12,7 +10,6 @@ import com.backendless.Backendless;
 import com.backendless.async.callback.AsyncCallback;
 import com.backendless.exceptions.BackendlessFault;
 import com.backendless.push.DeviceRegistrationResult;
-import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
 import org.threeten.bp.LocalDate;
@@ -20,20 +17,16 @@ import org.threeten.bp.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.fragment.app.Fragment;
 import edu.etzion.koletzion.Fragments.MainViewPagerFragment;
 import edu.etzion.koletzion.Fragments.MoodFragment;
 import edu.etzion.koletzion.authentication.AuthenticationActivity;
 import edu.etzion.koletzion.player.ExoPlayerFragment;
 import edu.etzion.koletzion.player.StartLiveStreamTask;
 
-public class MainActivity extends AppCompatActivity
-		implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 	
 	
 	private FirebaseAuth auth;
@@ -112,7 +105,6 @@ public class MainActivity extends AppCompatActivity
 		findViews();
 		startAuthenticationActivityIfNeeded();
 		initFragments();
-		initDrawer();
 	}
 	
 	private void initFragments() {
@@ -120,16 +112,6 @@ public class MainActivity extends AppCompatActivity
 				playerFragment).commit();
 		frame.setVisibility(View.GONE);
 		getSupportFragmentManager().beginTransaction().replace(R.id.contentMain, new MainViewPagerFragment()).commit();
-	}
-	
-	private void initDrawer() {
-		ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-				this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-		drawer.addDrawerListener(toggle);
-		toggle.syncState();
-		
-		NavigationView navigationView = findViewById(R.id.nav_view);
-		navigationView.setNavigationItemSelectedListener(this);
 	}
 	
 	
@@ -145,58 +127,11 @@ public class MainActivity extends AppCompatActivity
 		auth = FirebaseAuth.getInstance();
 		frame = findViewById(R.id.frame);
 		toolbar = findViewById(R.id.toolbar);
-		drawer = findViewById(R.id.drawer_layout);
 		playerFragment = new ExoPlayerFragment();
 		findViewById(R.id.btnLiveStream).setOnClickListener(this);
 		receiver = new PushNotificationReceiver(findViewById(R.id.btnLiveStream));
 	}
 	
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-	
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		
-		//noinspection SimplifiableIfStatement
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		
-		return super.onOptionsItemSelected(item);
-	}
-	
-	@Override
-	public boolean onNavigationItemSelected(MenuItem item) {
-		// Handle navigation view item clicks here.
-		int id = item.getItemId();
-		
-		if (id == R.id.homePage) {
-			// Handle the camera action
-			List<Fragment> fragments = getSupportFragmentManager().getFragments();
-			for (Fragment fragment : fragments) {
-				if (fragment instanceof MainViewPagerFragment) {
-					((MainViewPagerFragment) fragment).vpMain.setCurrentItem(2);
-					return true;
-				}
-			}
-			getSupportFragmentManager().beginTransaction().replace(R.id.contentMain, new MainViewPagerFragment()).commit();
-		} else if (id == R.id.logOut) {
-			auth.signOut();
-			playerFragment.stopPlayer();
-			startAuthenticationActivityIfNeeded();
-		}
-		DrawerLayout drawer = findViewById(R.id.drawer_layout);
-		drawer.closeDrawer(GravityCompat.START);
-		return true;
-	}
 	
 	public void initPlayer(String filePath) {
 		playerFragment.initPlayer(filePath);
@@ -216,7 +151,6 @@ public class MainActivity extends AppCompatActivity
 	
 	@Override
 	public void onClick(View v) {
-		MainActivity activity = this;
 		new StartLiveStreamTask(playerFragment).execute();
 //		v.setOnClickListener(null);
 //		final Handler handler = new Handler();
