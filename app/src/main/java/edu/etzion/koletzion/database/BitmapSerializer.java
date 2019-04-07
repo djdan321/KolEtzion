2a17afc1ff8cbe9a61f5c31caa7fa4bfaf7595cf
Fongeme <fridyair@gmail.com>
2019-04-07 22:42:10 +0300

    sas

--- app/src/main/java/edu/etzion/koletzion/database/BitmapSerializer.java
+++ app/src/main/java/edu/etzion/koletzion/database/BitmapSerializer.java
@@ -9,18 +9,28 @@ import android.widget.ImageView;
 import java.io.ByteArrayOutputStream;
 
 public class BitmapSerializer {
-	public static Bitmap decodeStringToBitmap(String encodedBitmap){
+	public static Bitmap decodeStringToBitmap(String encodedBitmap) {
 		byte[] decodedBytes = Base64.decode(encodedBitmap, 0);
 		return BitmapFactory.decodeByteArray(decodedBytes, 0, decodedBytes.length);
 	}
 	
-	public static String encodeBitmapToString(Bitmap bitmap){
+	public static String encodeBitmapToString(Bitmap bitmap) {
 		ByteArrayOutputStream byteArrayOS = new ByteArrayOutputStream();
-		bitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOS);
+		int i = 50;
+		bitmap.compress(Bitmap.CompressFormat.JPEG, i, byteArrayOS);
+		for (int j = 1; j < 10; j++) {
+			if (bitmap.getByteCount() == 700000) {
+				bitmap.compress(Bitmap.CompressFormat.JPEG, i, byteArrayOS);
+			} else if (bitmap.getByteCount() > 700000) {
+				bitmap.compress(Bitmap.CompressFormat.JPEG, (i /= (2 / j)), byteArrayOS);
+			} else if (bitmap.getByteCount() < 700000) {
+				bitmap.compress(Bitmap.CompressFormat.JPEG, (i *= (2 / j)), byteArrayOS);
+			}
+		}
 		return Base64.encodeToString(byteArrayOS.toByteArray(), Base64.DEFAULT);
 	}
 	
-	public static Bitmap getBitmapFromImageView(ImageView imageView){
-		return ((BitmapDrawable)imageView.getDrawable()).getBitmap();
+	public static Bitmap getBitmapFromImageView(ImageView imageView) {
+		return ((BitmapDrawable) imageView.getDrawable()).getBitmap();
 	}
 }
